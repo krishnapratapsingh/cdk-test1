8b3b93011bb8e556bd7dc1791adf44f82dcba649
krishna <krishan.singh@genpact.com>
2021-11-29 07:50:53 +0000

    file changes

--- bin/cdk-test1.ts
+++ bin/cdk-test1.ts
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from '@aws-cdk/core';
-import { MyPipelineStack } from '../lib/my-pipeline-stack';
+import { MyPipelineStack } from '../lib/cdk-test1-stack';
 
 const app = new cdk.App();
 new MyPipelineStack(app, 'MyPipelineStack', {
